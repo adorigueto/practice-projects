2cd3e4df77aee912b5f0bbe7d3f64799f5765e3e
André Canal <andredorigueto@gmail.com>
2025-09-23 10:22:00 -0300

    Second exercise: operations.

---
+++ operations.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def operations(h, w):
+    """
+    Takes two inputs, h and w, and makes two Numpy arrays A and B of size
+    h x w, and returns A, B, and s, the sum of A and B.
+
+    Arg:
+      h - an integer describing the height of A and B
+      w - an integer describing the width of A and B
+    Returns (in this order):
+      A - a randomly-generated h x w Numpy array.
+      B - a randomly-generated h x w Numpy array.
+      s - the sum of A and B.
+    """
+    A = np.random.random([h,w])
+    B = np.random.random([h,w])
+    s = A + B
+    return A, B, s
+
+A = operations(2,2)[0]
+B = operations(2,2)[1]
+s = operations(2,2)[2]
+
+print(A, '\n\n', B, '\n\n', s)
